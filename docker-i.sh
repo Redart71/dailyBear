05a40b64967841caa04ffba83b944cc1b03d7957
Redart71 <69314477+Redart71@users.noreply.github.com>
2023-01-09 21:45:54 +0100

    Update docker-i.sh

--- docker-i.sh
+++ docker-i.sh
@@ -6,8 +6,8 @@ apt-get update
 apt-get install ca-certificates curl gnupg lsb-release
 # updating packages
 mkdir -p /etc/apt/keyrings
+# create directory keyrings
 [ ! -f /etc/apt/keyrings/docker.gpg ] && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg || echo "Already docker image"
-
 #get docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
